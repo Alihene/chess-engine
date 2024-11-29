489005c8c19c4279185f8cc59ab6644b35e09439
ali hene <alihene222@gmail.com>
2024-11-29 19:53:12 +0000

    added minimax with alpha beta pruning

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <limits.h>
 
 typedef int8_t i8;
 typedef int16_t i16;
@@ -14,6 +15,9 @@ typedef uint32_t u32;
 typedef uint64_t u64;
 typedef float f32;
 typedef double f64;
+
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
 
 typedef enum {
     KING = 0x0,
@@ -48,10 +52,15 @@ typedef struct {
 Board board = {0};
 Board search_board = {0};
 
+u32 minimax_count = 0;
+
 Piece *get_piece(u32 x, u32 y, Board *b) {
     u32 index = x + y * 8;
+    if(index >= 64) {
+        return NULL;
+    }
     if(b->pieces_state & (1L << index)) {
-        return &b->pieces[x + y * 8];
+        return &b->pieces[index];
     }
 
     return NULL;
@@ -157,13 +166,92 @@ i32 evaluate_board(Board *b) {
     return white - black;
 }
 
+char piece_to_char(const Piece *piece) {
+    switch(piece->type) {
+        case KING:
+        {
+            if(!piece->color) {
+                return 'K';
+            } else {
+                return 'k';
+            }
+            break;
+        }
+        case PAWN:
+        {
+            if(!piece->color) {
+                return 'P';
+            } else {
+                return 'p';
+            }
+            break;
+        }
+        case BISHOP:
+        {
+            if(!piece->color) {
+                return 'B';
+            } else {
+                return 'b';
+            }
+            break;
+        }
+        case KNIGHT:
+        {
+            if(!piece->color) {
+                return 'N';
+            } else {
+                return 'n';
+            }
+            break;
+        }
+        case ROOK:
+        {
+            if(!piece->color) {
+                return 'R';
+            } else {
+                return 'r';
+            }
+            break;
+        }
+        case QUEEN:
+        {
+            if(!piece->color) {
+                return 'Q';
+            } else {
+                return 'q';
+            }
+            break;
+        }
+        default:
+        {
+            return '\0';
+            break;
+        }
+    }
+}
+
+void print_board(const Board *b) {
+    printf("-----------------\n");
+    for(i32 i = 56; i >= 0; i -= 8) {
+        printf("|");
+        for(u32 j = i; j < i + 8; j++) {
+            if(b->pieces_state & (1L << j)) {
+                printf("%c|", piece_to_char(&b->pieces[j]));
+            } else {
+                printf(" |");
+            }
+        }
+        printf("\n");
+    }
+    printf("-----------------\n");
+}
+
 void generate_moves_for_piece(u32 x, u32 y, u32 color_to_move, Board *b, Move *moves, u32 *move_count) {
-    if(!(b->pieces_state & (1L << (x + y * 8)))) {
+    const Piece *piece = get_piece(x, y, b);
+    if(!piece) {
         *move_count = 0;
         return;
     }
-
-    const Piece *piece = &b->pieces[x + y * 8];
     if(piece->color != color_to_move) {
         *move_count = 0;
         return;
@@ -243,31 +331,69 @@ void generate_moves_for_piece(u32 x, u32 y, u32 color_to_move, Board *b, Move *m
         {
             if(color_to_move == COLOR_WHITE) {
                 // One square
-                if(y < 7 && !(get_piece(x, y + 1, b) && get_piece(x, y + 1, b)->color == color_to_move)) {
+                if(y < 7 && !get_piece(x, y + 1, b)) {
                     move.to.y = y + 1;
                     moves[i] = move;
                     i++;
                 }
                 // Two squares
                 if(y == 1
-                    && !(get_piece(x, 2, b) && get_piece(x, 2, b)->color == color_to_move)
-                    && !(get_piece(x, 3, b) && get_piece(x, 3, b)->color == color_to_move)) {
+                    && !get_piece(x, 2, b)
+                    && !get_piece(x, 3, b)) {
                     move.to.y = 3;
+                    moves[i] = move;
+                    i++;
+                }
+                // Diagonal capture
+                if(y < 7
+                    && x < 7
+                    && get_piece(x + 1, y + 1, b)
+                    && get_piece(x + 1, y + 1, b)->color != color_to_move) {
+                    move.to.x = x + 1;
+                    move.to.y = y + 1;
+                    moves[i] = move;
+                    i++;
+                }
+                if(y < 7
+                    && x > 0
+                    && get_piece(x - 1, y + 1, b)
+                    && get_piece(x - 1, y + 1, b)->color != color_to_move) {
+                    move.to.x = x - 1;
+                    move.to.y = y + 1;
                     moves[i] = move;
                     i++;
                 }
             } else {
                 // One square
-                if(y > 0 && !(get_piece(x, y - 1, b) && get_piece(x, y - 1, b)->color == color_to_move)) {
+                if(y > 0 && !get_piece(x, y - 1, b)) {
                     move.to.y = y - 1;
                     moves[i] = move;
                     i++;
                 }
                 // Two squares
                 if(y == 6
-                    && !(get_piece(x, 5, b) && get_piece(x, 5, b)->color == color_to_move)
-                    && !(get_piece(x, 4, b) && get_piece(x, 4, b)->color == color_to_move)) {
+                    && !get_piece(x, 5, b)
+                    && !get_piece(x, 4, b)) {
                     move.to.y = 4;
+                    moves[i] = move;
+                    i++;
+                }
+                // Diagonal capture
+                if(y > 0
+                    && x < 7
+                    && get_piece(x + 1, y - 1, b)
+                    && get_piece(x + 1, y - 1, b)->color != color_to_move) {
+                    move.to.x = x + 1;
+                    move.to.y = y - 1;
+                    moves[i] = move;
+                    i++;
+                }
+                if(y > 0
+                    && x > 0
+                    && get_piece(x - 1, y - 1, b)
+                    && get_piece(x - 1, y - 1, b)->color != color_to_move) {
+                    move.to.x = x - 1;
+                    move.to.y = y - 1;
                     moves[i] = move;
                     i++;
                 }
@@ -703,118 +829,117 @@ void generate_moves_for_piece(u32 x, u32 y, u32 color_to_move, Board *b, Move *m
 }
 
 // out_moves must be size 256
-void generate_moves(u32 color_to_move, Move *out_moves, u32 *count, Board *b) {
-    Move moves[256];
+Move *generate_moves(i32 color_to_move, u32 *count, Board *b) {
+    // TODO: bump allocator?
+    Move *out_moves = malloc(256 * sizeof(Move));
+    if(!out_moves) {
+        fprintf(stderr, "Failed to allocate 256 move buffer\n");
+        exit(-1);
+        return NULL;
+    }
     u32 move_count = 0;
 
     for(u32 x = 0; x < 8; x++) {
         for(u32 y = 0; y < 8; y++) {
             u32 m = 0;
-            generate_moves_for_piece(x, y, COLOR_WHITE, b, moves, &m);
+            generate_moves_for_piece(x, y, color_to_move, b, &out_moves[move_count], &m);
             move_count += m;
         }
     }
 
-    if(out_moves) {
-        memcpy(out_moves, moves, sizeof(moves));
-    }
     if(count) {
         *count = move_count;
     }
+
+    return out_moves;
 }
 
-i32 negamax(u32 depth) {
+i32 minimax(Board *b, i32 depth, i32 alpha, i32 beta, i32 who_to_move) {
+    minimax_count++;
+
     if(depth == 0) {
-        
+        return evaluate_board(b);
     }
-}
 
-char piece_to_char(const Piece *piece) {
-    switch(piece->type) {
-        case KING:
-        {
-            if(!piece->color) {
-                return 'K';
-            } else {
-                return 'k';
+    u32 move_count = 0;
+    if(who_to_move == COLOR_WHITE) {
+        i32 max_eval = INT_MIN;
+        Move *minimax_moves = generate_moves(who_to_move, &move_count, b);
+        for(u32 i = 0; i < move_count; i++) {
+            Piece *p = get_piece(minimax_moves[i].to.x, minimax_moves[i].to.y, b);
+            bool piece_exists = p;
+            Piece p_copy;
+            if(p) {
+                p_copy = *p;
             }
-            break;
-        }
-        case PAWN:
-        {
-            if(!piece->color) {
-                return 'P';
-            } else {
-                return 'p';
+            move_piece(
+                minimax_moves[i].from.x,
+                minimax_moves[i].from.y,
+                minimax_moves[i].to.x,
+                minimax_moves[i].to.y,
+                b);
+            i32 eval = minimax(b, depth - 1, alpha, beta, COLOR_BLACK);
+            max_eval = MAX(max_eval, eval);
+            move_piece(
+                minimax_moves[i].to.x,
+                minimax_moves[i].to.y,
+                minimax_moves[i].from.x,
+                minimax_moves[i].from.y,
+                b);
+            if(piece_exists) {
+                set_piece(minimax_moves[i].to.x, minimax_moves[i].to.y, &p_copy, b);
             }
-            break;
-        }
-        case BISHOP:
-        {
-            if(!piece->color) {
-                return 'B';
-            } else {
-                return 'b';
+            alpha = MAX(alpha, eval);
+            if(eval >= beta) {
+                break;
             }
-            break;
         }
-        case KNIGHT:
-        {
-            if(!piece->color) {
-                return 'N';
-            } else {
-                return 'n';
+        free(minimax_moves);
+        return max_eval;
+    } else {
+        i32 min_eval = INT_MAX;
+        Move *minimax_moves = generate_moves(who_to_move, &move_count, b);
+        for(u32 i = 0; i < move_count; i++) {
+            Piece *p = get_piece(minimax_moves[i].to.x, minimax_moves[i].to.y, b);
+            bool piece_exists = p;
+            Piece p_copy;
+            if(p) {
+                p_copy = *p;
             }
-            break;
-        }
-        case ROOK:
-        {
-            if(!piece->color) {
-                return 'R';
-            } else {
-                return 'r';
+            move_piece(
+                minimax_moves[i].from.x,
+                minimax_moves[i].from.y,
+                minimax_moves[i].to.x,
+                minimax_moves[i].to.y,
+                b);
+            i32 eval = minimax(b, depth - 1, alpha, beta, COLOR_WHITE);
+            min_eval = MIN(min_eval, eval);
+            move_piece(
+                minimax_moves[i].to.x,
+                minimax_moves[i].to.y,
+                minimax_moves[i].from.x,
+                minimax_moves[i].from.y,
+                b);
+            if(piece_exists) {
+                set_piece(minimax_moves[i].to.x, minimax_moves[i].to.y, &p_copy, b);
             }
-            break;
-        }
-        case QUEEN:
-        {
-            if(!piece->color) {
-                return 'Q';
-            } else {
-                return 'q';
+            beta = MIN(beta, eval);
+            if(eval <= alpha) {
+                break;
             }
-            break;
         }
-        default:
-        {
-            return '\0';
-            break;
-        }
+        free(minimax_moves);
+        return min_eval;
     }
-}
-
-void print_board(const Board *b) {
-    printf("-----------------\n");
-    for(i32 i = 56; i >= 0; i -= 8) {
-        printf("|");
-        for(u32 j = i; j < i + 8; j++) {
-            if(b->pieces_state & (1L << j)) {
-                printf("%c|", piece_to_char(&b->pieces[j]));
-            } else {
-                printf(" |");
-            }
-        }
-        printf("\n");
-    }
-    printf("-----------------\n");
 }
 
 int main() {
     setup_board(&board);
     memcpy(&search_board, &board, sizeof(board));
 
-    Move moves[256];
-    generate_moves(COLOR_WHITE, NULL, NULL, &board);
+    i32 eval = minimax(&search_board, 6, INT_MIN, INT_MAX, COLOR_WHITE);
+    printf("Eval: %i\n", eval);
+    printf("Number of minimax calls: %u\n", minimax_count);
 
     print_board(&board);
     return 0;
